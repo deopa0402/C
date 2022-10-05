14b16aee03b331b2d8e9eb87bfd3b25f35771180
deopa0402 <deopa0402@gmail.com>
2022-10-05 16:07:06 +0900

    two_arrey

---
+++ two_arrey.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int arr[2][3] = {0};
+    int i,k ;
+for ( i = 0; i < 2; i++)
+{
+    for ( k = 0; k < 3; k++)
+    {
+        printf("%d\n",arr[i][k]);
+        /* code */
+    }
+    
+    /* code */
+}
+
+
+}
